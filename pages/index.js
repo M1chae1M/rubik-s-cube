645a6ac852acb8b56f39f3ad5abfbdfc44a4b13e
M1chae1M <michalmaru10@gmail.com>
2023-04-12 20:19:43 +0200

    new front animation

--- pages/index.js
+++ pages/index.js
@@ -116,38 +116,22 @@ export default class App extends Component{
       },speed);
     }
     const wallEditTrans=(name)=>{
+      const oneTo8=[0,1,2,3,4,5,6,7,8];
       const queryName=document?.querySelectorAll(`.${name}`);
 
       const oldTransition=queryName[0].style.transition;
       const copyStyles=(obj)=>{return queryName[obj].style.transform};
-      const copied={
-        o1:copyStyles(0),
-        o2:copyStyles(1),
-        o3:copyStyles(2),
-        o4:copyStyles(3),
-        o5:copyStyles(4),
-        o6:copyStyles(5),
-        o7:copyStyles(6),
-        o8:copyStyles(7),
-        o9:copyStyles(8),
-      }
+      const copied=[];
+      oneTo8.map(x=>copied.push(copyStyles(x)))
       setTimeout(()=>{
-        [0,1,2,3,4,5,6,7,8].map(x=>queryName[x].style.transition='none');
+        oneTo8.map(x=>queryName[x].style.transition='none');
 
-        queryName[0].style.transform=copied.o1;
-        queryName[1].style.transform=copied.o2;
-        queryName[2].style.transform=copied.o3;
-        queryName[3].style.transform=copied.o4;
-        queryName[4].style.transform=copied.o5;
-        queryName[5].style.transform=copied.o6;
-        queryName[6].style.transform=copied.o7;
-        queryName[7].style.transform=copied.o8;
-        queryName[8].style.transform=copied.o9;
+        oneTo8.map(x=>queryName[x].style.transform=copied[x]);
 
         setTimeout(()=>{
-          [0,1,2,3,4,5,6,7,8].map(x=>queryName[x].style.transition=oldTransition);
+          oneTo8.map(x=>queryName[x].style.transition=oldTransition);
         },100);
-      },speed);
+    },speed);
     }
     const spinHoriz=(row)=>{
       const animate=(row)=>{
@@ -287,9 +271,28 @@ export default class App extends Component{
               break;
             }
             case 'front':{
-              addStyles(o1,`rotate(90deg) translateY(${+move}px) translateX(${move}px)`);
-              addStyles(o2,`rotate(90deg) translateY(${50+move}px) translateX(${-50+move}px)`);
-              addStyles(o3,`rotate(90deg) translateY(${100+move}px) translateX(${-100+move}px)`);
+              wallEditTrans(name);
+
+              queryName[0].style.transform=`translateX(100px)`;
+              queryName[1].style.transform=`translateY(0px) translateX(50px)`;
+              queryName[2].style.transform=`translateY(0px)`;
+              queryName[3].style.transform=`translateX(100px) translateY(50px)`;
+              queryName[5].style.transform=`translateX(0px) translateY(50px)`;
+              queryName[6].style.transform=`translateX(100px) translateY(100px)`;
+              queryName[7].style.transform=`translateX(50px) translateY(100px)`;
+              queryName[8].style.transform=`translateX(0px) translateY(100px)`;
+
+              // addStyles(0,`translateX(-50px)`);
+              // addStyles(1,`translateX(-50px)`);
+              // queryName[0].style.transform+=`translateX(50px)`;
+              // queryName[1].style.transform+=`translateX(0px)`;
+              // queryName[2].style.transform=`translateY(0px)`;
+              // queryName[3].style.transform=`translateX(100px) translateY(50px)`;
+              // queryName[5].style.transform=`translateX(0px) translateY(50px)`;
+              // queryName[6].style.transform=`translateX(100px) translateY(100px)`;
+              // queryName[7].style.transform=`translateX(50px) translateY(100px)`;
+              // queryName[8].style.transform=`translateX(0px) translateY(100px)`;
+              
               break;
             }
             case 'back':{
@@ -322,8 +325,8 @@ export default class App extends Component{
           coreAnimation('left',0,6,7,8);
           coreAnimation('bot',0,0,3,6);
           coreAnimation('front',0,0,1,2);
-          coreAnimation('front',50,3,4,5);
-          coreAnimation('front',100,6,7,8);
+          // coreAnimation('front',50,3,4,5);
+          // coreAnimation('front',100,6,7,8);
         }
       }
       if(this.state.canMix){
@@ -437,14 +440,14 @@ export default class App extends Component{
             }
             case 'right':{
               wallEditTrans(name);
-              queryName[0].style.transform+=`translateY(100px)`
-              queryName[1].style.transform+=`translateY(50px) translateX(50px)`
-              queryName[2].style.transform+=`translateX(100px)`
-              queryName[3].style.transform+=`translateX(-50px) translateY(50px)`
-              queryName[5].style.transform+=`translateX(50px) translateY(-50px)`
-              queryName[6].style.transform+=`translateX(-100px)`
-              queryName[7].style.transform+=`translateY(-50px) translateX(-50px)`
-              queryName[8].style.transform+=`translateY(-100px)`
+              queryName[0].style.transform=`translateY(100px)`
+              queryName[1].style.transform=`translateY(50px) translateX(50px)`
+              queryName[2].style.transform=`translateX(100px)`
+              queryName[3].style.transform=`translateX(-50px) translateY(50px)`
+              queryName[5].style.transform=`translateX(50px) translateY(-50px)`
+              queryName[6].style.transform=`translateX(-100px)`
+              queryName[7].style.transform=`translateY(-50px) translateX(-50px)`
+              queryName[8].style.transform=`translateY(-100px)`
               break;
             }
           }
